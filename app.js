49b45152a3cc95e1456f1147c5e2a15358d60f56
muskanjais30 <121816807+muskanjais30@users.noreply.github.com>
2024-03-31 01:12:12 +0530

    integrating

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use(express.json());
 app.use(bodyParser.json());
 
