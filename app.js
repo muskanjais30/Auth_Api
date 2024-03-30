75ee9d8e66eb0183a491bec9a1712ed5b61697af
muskanjais30 <121816807+muskanjais30@users.noreply.github.com>
2024-03-30 21:47:45 +0530

    integrating

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
+const cors = require("cors")
 
 require('dotenv').config();
 
